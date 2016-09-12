41c40158630b614b3d4865537074035abcb851ec
Linkway <manuel@linkway.me>
2016-09-12 18:07:03 -0300

    Add files via upload

---
+++ js/wildfire.js
@@ -0,0 +1,15 @@
+    if (typeof MauticSDKLoaded == 'undefined') {
+        var MauticSDKLoaded = true;
+        var head            = document.getElementsByTagName('head')[0];
+        var script          = document.createElement('script');
+        script.type         = 'text/javascript';
+        script.src          = 'http://85.17.164.138/~linkway/uxtest/mautic/media/js/mautic-form.js';
+        script.onload       = function() {
+            MauticSDK.onLoad();
+        };
+        head.appendChild(script);
+        var MauticDomain = 'http://85.17.164.138';
+        var MauticLang   = {
+            'submittingMessage': "Un segundo..."
+        }
+    }
